22baa2e82d1bc6c73de4b889ba7994a9bab9fda8
epmt6528 <epmt6528@gmail.com>
2023-09-05 12:29:46 -0700

    Set up ChakraProvider

---
+++ src/styles/theme.ts
@@ -0,0 +1,44 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    moji: {
+      main: '#030D1B',
+      sub: '#A09E9F',
+    },
+    solid: {
+      main: '#DEDCDB',
+    },
+    brand: {
+      main: '#4B95C2',
+      mark: '#D8B189',
+    },
+  },
+  fontSizes: {
+    xmini: '0.7em',
+    mini: '0.8em',
+    base: '16px',
+    big: '1.2em',
+  },
+  borders: {
+    main: '1px solid #DEDCDB',
+  },
+  styles: {
+    global: {
+      'html, body': {
+        color: 'moji.main',
+        background: '#fff',
+        lineHeight: '1.4142135623',
+        fontSize: 'base',
+        letterSpacing: '0.08em',
+        textAlign: 'justify',
+        textJustify: 'inter-ideograph',
+        fontFamily:
+          '"Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", "BIZ UDPGothic",Meiryo, sans-serif',
+      },
+      a: {
+        color: 'brand.main',
+      },
+    },
+  },
+});
