d21920b4b0b781b7aa4d540c9cd1de9e719b0a92
epmt6528 <epmt6528@gmail.com>
2023-09-06 20:15:29 -0700

    Created a base component for Landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './styles/App.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from './styles/theme.ts';
+import Landing from '@components/Landing/Landing.tsx';
 
 function App() {
-  return <ChakraProvider theme={theme}>TEST</ChakraProvider>;
+  return (
+    <ChakraProvider theme={theme}>
+      <Landing />
+    </ChakraProvider>
+  );
 }
 
 export default App;
