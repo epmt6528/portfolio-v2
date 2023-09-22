ab4299350b4c483d3a45f2dce27448ca10669fc3
epmt6528 <epmt6528@gmail.com>
2023-09-21 21:32:02 -0700

    Created History page base

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from './styles/theme.ts';
 import Landing from './components/Landing/Landing.tsx';
+import History from './components/History/History.tsx';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Landing />
+      <History />
     </ChakraProvider>
   );
 }
